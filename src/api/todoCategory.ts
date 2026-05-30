26ba09982a11fa9bdc7c1a5a365129cad86116cd
niutengfei <niutengfei@magiccompass.cn>
2026-05-30 19:48:06 +0800

    feat: add todoCategory API client, add category field to Todo

---
+++ src/api/todoCategory.ts
@@ -0,0 +1,17 @@
+import { http } from './http';
+
+export interface TodoCategory {
+  id: number;
+  name: string;
+}
+
+export const todoCategoryApi = {
+  getAll: (): Promise<TodoCategory[]> =>
+    http.get<TodoCategory[]>('/api/todo-categories').then((r) => r.data),
+
+  create: (name: string): Promise<TodoCategory> =>
+    http.post<TodoCategory>('/api/todo-categories', { name }).then((r) => r.data),
+
+  remove: (id: number): Promise<void> =>
+    http.delete(`/api/todo-categories/${id}`).then(() => undefined),
+};
